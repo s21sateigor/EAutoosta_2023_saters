6f079b97dbe3a0a2378d4fccbe4441de7f7d7f45
Igors Saters <s21sateigor@venta.lv>
2023-04-07 22:31:40 +0300

    New: Cashier class completed

--- src/model/Cashier.java
+++ src/model/Cashier.java
@@ -3,11 +3,34 @@ package model;
 import java.time.LocalDate;
 
 public class Cashier extends Employee {
-    private static int cashierID;
+    private int id;
+    private static int cashierID = 1;
 
 
-    public Cashier(String name, String surname, String personCode, LocalDate contractDate, int cashierID) {
+    public Cashier(){
+        super();
+        id = cashierID;
+        cashierID++;
+    }
+
+    public Cashier(String name, String surname, String personCode, LocalDate contractDate) throws Exception {
         super(name, surname, personCode, contractDate);
-        this.cashierID = cashierID;
+        id = cashierID;
+        cashierID++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Cashier{" +
+                "id=" + id +
+                '}';
     }
 }
